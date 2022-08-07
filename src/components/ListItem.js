1e63e02de7ba4c4396e2c3fa2076d2b2a2e73446
Kinga Zyborowicz <kinzyb@wp.pl>
2022-08-07 21:29:24 +0200

    Add checkbox

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const ListItem = ({ item }) => {
-  const { id, from, sent_date, subject, snippet } = item;
+const ListItem = ({ item, handleChange }) => {
+  const { id, from, sent_date, subject, snippet, is_unread } = item;
 
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={is_unread}
+        onChange={() => handleChange(id)}
+      />
       {id} {from} {sent_date} {subject} {snippet}
     </li>
   );
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,9 +1,21 @@
+import React, { useState } from "react";
+
 import { mockData } from "../mockData";
 import ListItem from "./ListItem";
 
 const ListView = () => {
-  const list = mockData.map((item) => {
-    return <ListItem key={item.id} item={item} />;
+  const [data, setData] = useState(mockData);
+
+  const handleChange = (idParameter) => {
+    setData((prevData) =>
+      prevData.map((item) =>
+        item.id === idParameter ? { ...item, is_unread: !item.is_unread } : item
+      )
+    );
+  };
+
+  const list = data.map((item) => {
+    return <ListItem key={item.id} item={item} handleChange={handleChange} />;
   });
 
   return <ul>{list}</ul>;
